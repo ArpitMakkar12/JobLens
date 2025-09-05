0a815613edef9d5a36562393a24cc730b7ab974d
ArpitMakkar12 <arpitmakkar12@gmail.com>
2025-09-06 00:33:42 +0530

    NEW

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://skill-sync-backend.onrender.com/analyze'; // ⬅️ Your URL from Render
+const BASE_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${BASE_URL}/analyze`;
 
 // Define the shape of the data your API will return
 export interface AnalysisResult {
